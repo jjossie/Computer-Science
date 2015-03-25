f5c8d3b9518a83220347930fd69c84e2fb78ab70
jjossie <joeljossie@gmail.com>
2015-03-25 09:18:30 -0500

    Finished Main, maybe errors

--- src/Assignment5/Main.java
+++ src/Assignment5/Main.java
@@ -12,9 +12,33 @@ public class Main {
 			hand2.add(deck.getTopCard());
 		}
 		//Calculate hand 1 value
+		int hand1Value = 0;
 		for (int i = 0; i<hand1.size(); i++){
-
+			Card currentCard = hand1.get(i);
+			hand1Value += currentCard.pointValue();
 		}
+		//Calculate hand 1 value
+		int hand2Value = 0;
+		for (int i = 0; i<hand2.size(); i++){
+			Card currentCard = hand2.get(i);
+			hand2Value += currentCard.pointValue();
+		}
+
+		//Print Stuff
+		System.out.println("Hand 1: total points: " + hand1Value);
+		for (int i = 0; i<hand1.size(); i++){
+			System.out.println(hand1.get(i));
+		}
+		System.out.println("\nHand 2: total points: " + hand2Value);
+		for (int i = 0; i<hand2.size(); i++){
+			System.out.println(hand2.get(i));
+		}
+		if(hand1Value<hand2Value){
+			System.out.println("Hand 2 wins");
+		}else if(hand2Value<hand1Value){
+			System.out.println("Hand 1 wins");
+		}else
+			System.out.println("Tie");
 	}
 
 }

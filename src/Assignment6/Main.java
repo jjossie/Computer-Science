67647d7d9fb31df49b5d847b4b80589b65e3f642
jjossie <joeljossie@gmail.com>
2015-03-28 16:27:30 -0500

    Assignment 6 started

---
+++ src/Assignment6/Main.java
@@ -0,0 +1,28 @@
+package Assignment6;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Main {
+	static ArrayList<String> names = new ArrayList<String>();
+
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		String in = "poop";
+		while(!in.toUpperCase().equals("STOP")){
+			System.out.println("Enter the next name:");
+			in = s.nextLine();
+			if(!in.toUpperCase().equals("STOP")){
+				names.add(titleCase(in));
+			}
+		}
+		System.out.print(names.toString());
+	}
+
+	public static String titleCase(String s){
+		String n = "";
+		n.toCharArray()[0] = s.toUpperCase().charAt(0);
+		for(int i = 1; i<s.length(); i++){
+			n.toCharArray()[i] = s.toLowerCase().charAt(i);
+		}
+		return n;
+	}
+}
